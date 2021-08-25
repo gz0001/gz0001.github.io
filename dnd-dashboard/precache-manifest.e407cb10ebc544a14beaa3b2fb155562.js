1da96a92c3b78ccbf435d999e246a69026e22ae6
gz0001 <progger116@gmail.com>
2021-08-25 15:45:04 +0200

    chang dashboard url

---
+++ dnd-dashboard/precache-manifest.e407cb10ebc544a14beaa3b2fb155562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f28ecca855f36a38c86",
+    "url": "/dnd-dashboard/static/css/main.e55e7df6.chunk.css"
+  },
+  {
+    "revision": "4f28ecca855f36a38c86",
+    "url": "/dnd-dashboard/static/js/main.4f28ecca.chunk.js"
+  },
+  {
+    "revision": "b282df77e86aa0bce545",
+    "url": "/dnd-dashboard/static/css/1.dabd06b5.chunk.css"
+  },
+  {
+    "revision": "b282df77e86aa0bce545",
+    "url": "/dnd-dashboard/static/js/1.b282df77.chunk.js"
+  },
+  {
+    "revision": "090d34ee1227e2c705f4",
+    "url": "/dnd-dashboard/static/js/runtime~main.090d34ee.js"
+  },
+  {
+    "revision": "b72090ef983ef78d82838fbd15de36b6",
+    "url": "/dnd-dashboard/static/media/icomoon.b72090ef.woff"
+  },
+  {
+    "revision": "f7c7431532092de78227abfcb253e7a1",
+    "url": "/dnd-dashboard/index.html"
+  }
+];
--- dnd-dashboard/service-worker.js
+++ dnd-dashboard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95e2af7a5034853299e91c75a5c43434.js"
+  "/dnd-dashboard/precache-manifest.e407cb10ebc544a14beaa3b2fb155562.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dnd-dashboard/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
