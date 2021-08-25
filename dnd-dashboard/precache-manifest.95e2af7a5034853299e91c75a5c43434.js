32fb709589f06d580300b92338f169bf164e654a
gz0001 <progger116@gmail.com>
2021-08-25 15:28:42 +0200

    add dashboard example

---
+++ dnd-dashboard/precache-manifest.95e2af7a5034853299e91c75a5c43434.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86d39450dbee94167ce4",
+    "url": "/static/css/main.037c8152.chunk.css"
+  },
+  {
+    "revision": "86d39450dbee94167ce4",
+    "url": "/static/js/main.86d39450.chunk.js"
+  },
+  {
+    "revision": "b282df77e86aa0bce545",
+    "url": "/static/css/1.dabd06b5.chunk.css"
+  },
+  {
+    "revision": "b282df77e86aa0bce545",
+    "url": "/static/js/1.b282df77.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b72090ef983ef78d82838fbd15de36b6",
+    "url": "/static/media/icomoon.b72090ef.woff"
+  },
+  {
+    "revision": "f5790616502be38cb55ae034904ea75d",
+    "url": "/index.html"
+  }
+];
